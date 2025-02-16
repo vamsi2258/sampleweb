3aa7c31b808a1e04755c9c11f1cb0f1f267546b6
vamsi2258 <vamsi2337@gmail.com>
2025-02-17 02:19:44 +0530

    --added sample project

---
+++ src/main/java/com/bluetree/ems/bluetreeEmsAppApplication.java
@@ -0,0 +1,13 @@
+package com.bluetree.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bluetreeEmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(bluetreeEmsAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bluetree/ems/model/Employee.java
@@ -0,0 +1,97 @@
+package com.bluetree.ems.model;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;	
+	
+	@Column(name = "email")
+	private String email;
+	
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Temporal(TemporalType.DATE)
+	@Column(name = "dob")
+	private Date dob;
+
+	@Column(name = "age")
+	private int age;
+
+	@Column(name = "salary")
+	private double salary;
+
+	@Column(name = "status")
+	private boolean status;
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+
+}
